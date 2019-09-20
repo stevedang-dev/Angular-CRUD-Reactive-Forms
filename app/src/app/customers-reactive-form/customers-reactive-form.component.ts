13a3e431bc92217a52c77434c931f7b8e1bbd11d
Steve Dang <stevedang.dev@gmail.com>
2019-09-20 17:38:00 -0400

    Form Validation

--- app/src/app/customers-reactive-form/customers-reactive-form.component.ts
+++ app/src/app/customers-reactive-form/customers-reactive-form.component.ts
@@ -1,6 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../customers/customer';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import {
+    FormGroup,
+    FormBuilder,
+    Validators,
+    AbstractControl,
+    ValidatorFn
+} from '@angular/forms';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+        if (
+            c.value !== null &&
+            (isNaN(c.value) || c.value < min || c.value > max)
+        ) {
+            return { range: true };
+        }
+        return null;
+    };
+}
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+    const emailControl = c.get('email');
+    const confirmationControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmationControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmationControl.value) {
+        return null;
+    }
+    return { match: true };
+}
 
 @Component({
     selector: 'app-customers-reactive-form',
@@ -15,11 +44,30 @@ export class CustomersReactiveFormComponent implements OnInit {
 
     ngOnInit() {
         this.customerForm = this.fb.group({
-            firstName: 'Steve',
-            lastName: { value: 'n/a', disabled: true },
-            email: '',
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            emailGroup: this.fb.group(
+                {
+                    email: ['', [Validators.required, Validators.email]],
+                    confirmEmail: ['', Validators.required]
+                },
+                { validator: emailMatcher }
+            ),
+            phone: '',
+            notification: 'email',
+            rating: [null, ratingRange(1, 5)],
             sendCatalog: true
         });
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
 
     // Patch Value
